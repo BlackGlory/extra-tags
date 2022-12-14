5970363a7fa5c0acd5e13ba82b18fbd9c4cd0c1c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-12-14 05:07:46 +0000

    refactor: use @blackglory/prelude

--- src/transformers/remove-falsy-values.ts
+++ src/transformers/remove-falsy-values.ts
@@ -1,14 +1,10 @@
 import { filter } from '@operators/filter'
 import { TagParameters } from '@src/types'
-import { Falsy } from '@blackglory/prelude'
+import { isntFalsy, Falsy } from '@blackglory/prelude'
 
 export function removeFalsyValues<T>(
   strings: TemplateStringsArray
 , ...values: T[]
 ): TagParameters<Exclude<T, Falsy>> {
-  return filter<T, Exclude<T, Falsy>>(
-    x => !!x
-  , strings
-  , ...values
-  )
+  return filter<T, Exclude<T, Falsy>>(isntFalsy, strings, ...values)
 }
