ead74e9f96b124c2daff2299958da76588562f08
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2024-12-23 15:10:45 +0000

    test(array): add cases

--- __tests__/array.spec.ts
+++ __tests__/array.spec.ts
@@ -1,8 +1,22 @@
-import { test, expect } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { array } from '@src/array.js'
 
-test('array', () => {
-  const result = array`a${1}b${2}c`
+describe('array', () => {
+  test('string only', () => {
+    const result = array`foo`
 
-  expect(result).toStrictEqual(['a', 1, 'b', 2, 'c'])
+    expect(result).toStrictEqual(['foo'])
+  })
+
+  test('value only', () => {
+    const result = array`${1}`
+
+    expect(result).toStrictEqual(['', 1, ''])
+  })
+
+  test('string and value', () => {
+    const result = array`a${1}b${2}c`
+
+    expect(result).toStrictEqual(['a', 1, 'b', 2, 'c'])
+  })
 })
